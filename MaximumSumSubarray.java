f1a1ba814dfe25cf5d7238da822c1517c3ceec3f
veer123-ops <72941982+veer123-ops@users.noreply.github.com>
2025-11-02 23:33:28 +0530

    megersorted

--- MaximumSumSubarray.java
+++ MaximumSumSubarray.java
@@ -3,17 +3,39 @@ package veer.com;
 public class MaximumSumSubarray {
 
     public static int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
+       /* int maxSoFar = nums[0];
         int currentMax = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
             currentMax = Math.max(nums[i], currentMax + nums[i]);
             maxSoFar = Math.max(maxSoFar, currentMax);
-        }
+        	/*maxSoFar+=nums[i];
+        	if(maxSoFar>currentMax)
+        		currentMax=maxSoFar;
+        	if(maxSoFar<0) maxSoFar=0;*/
+        //}
 
-        return maxSoFar;
+       // return maxSoFar;
+       // return currentMax;
+    	
+    	//SECOND LOGIC
+    	
+    	int sum=0;
+    	int maxsum=nums[0];
+    	
+    	for(int i=0;i<nums.length;i++) {
+    		sum+=nums[i];
+    		
+    		if(sum>maxsum) maxsum=sum;
+    		if(sum<0) sum=0;
+    		
+    		
+    	}
+		return maxsum;
     }
 
+    
+   
     public static void main(String[] args) {
         // Input 1
         int[] nums1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
